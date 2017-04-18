a92598add70e764d7f0a6c0e528767caf665e102
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-04-18 16:11:24 +0200

    Initial version

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/calendar', function () {
+    return view('calendar');
+});
+
 Route::group(['middleware' => 'auth'], function () {
     //    Route::get('/link1', function ()    {
 //        // Uses Auth Middleware
@@ -22,4 +26,9 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('prova', function ()    {
+        $data = [];
+        return view('prova',$data);
+    })->name('prova');
+
 });
